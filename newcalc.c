73caefe74845dfed4ba5ffb3a8f336ed679e69b6
edg3012 <lorenzocruzedgareduardo30@gmail.com>
2024-09-27 14:59:37 -0600

    Create newcalc.c

---
+++ newcalc.c
@@ -0,0 +1,51 @@
+\*Edgar Eduardo Lorenzo Cuz
+ejercicio 4 calculadora con funciones
+
+
+#include <stadio.h>
+#include <match.h>
+
+int rep;
+
+int suma (int a, intb)
+{
+resp = (a+ b)
+return(resp);
+}
+
+int suma2 (int a; int b)
+{
+return(resp);
+}
+
+int resta(int a, int b)
+{
+resp = a - b;
+return(resp);
+}
+
+int mult(int a, int b)
+{
+resp = a * b;
+return(resp);
+}
+
+int div(int a, int b)
+{
+resp = a / b;
+return(resp);
+}
+
+int exp(int a, int b)
+{
+resp = pow(a,b);
+return(resp);
+}
+
+int main()
+{
+  int a = 0, b =0;
+  int respuestas;
+
+  printf(“Dime que operaacion deseas realizar ”)
+
