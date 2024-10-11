f1b13841d21d6e7771f1b019323b7c09b1569191
edg3012 <lorenzocruzedgareduardo30@gmail.com>
2024-10-10 23:28:30 -0600

    newcalc.c

--- newcalc.c
+++ newcalc.c
@@ -1,41 +1,77 @@
-\*Edgar Eduardo Lorenzo Cuz
+/*Edgar Eduardo Lorenzo Cuz
 ejercicio 4 calculadora con funciones
+practica 7*/
 
+#include <stdio.h>
+#include <math.h>
 
-#include <stadio.h>
-#include <match.h>
+int resp;
 
-int rep;
-
-int suma (int a, intb)
+int suma(int a, int b)
 {
-resp = (a+ b)
-return(resp);
+  resp = a + b;
+  return(resp);
 }
-
-int suma2 (int a; int b)
-{
-return(resp);
-}
-
 int resta(int a, int b)
 {
-resp = a - b;
-return(resp);
+  resp = a - b;
+  return (resp);
 }
-
 int mult(int a, int b)
 {
-resp = a * b;
-return(resp);
+  resp = a * b;
+  return (resp);
 }
-
 int div(int a, int b)
 {
-resp = a / b;
-return(resp);
+  resp = a / b;
+  return(resp);
+}
+int exp(int a, int b)
+{
+  resp = pow(a,b);
+  return(resp);
 }
 
+int main()
+{
+  int a = 0, b = 0;
+  int respuesta, opcion;
+
+  printf("Dime que operacion deseas realizar: \n");
+  printf("1 .- Suma\n");
+  printf("2 .- Resta\n");
+  printf("3 .- Multiplicacion\n");
+  printf("4 .- Division\n");
+  printf("5 .- Exponente\n");
+  scanf("%d", &option);
+
+  }
+  switch(opcion)
+  {
+    default:
+      respuesta = 0;
+      break;
+    case 1:
+      respuesta = suma(a,b);
+      break;
+    case 2:
+      respuesta = resta(a,b);
+      break;
+    case 3:
+      respuesta = mult(a,b);
+      break;
+    case 4:
+      respuesta = div(a,b);
+      break;
+    case 5:
+      respuesta = exp(a,b);
+      break;
+  }
+
+  printf("El resultado de la operacion es: %d\n", respuesta);
+  return 0;
+}
 int exp(int a, int b)
 {
 resp = pow(a,b);
@@ -47,5 +83,34 @@ int main()
   int a = 0, b =0;
   int respuestas;
 
-  printf(“Dime que operaacion deseas realizar ”)
-
+  printf("Dime que operacion deseas realizar: \n");
+  printf("1 .- Suma\n");
+  printf("2 .- Resta\n");
+  printf("3 .- Multiplicacion\n");
+  printf("4 .- Division\n");
+  printf("5 .- Exponente\n");
+  scanf("%d", &option);
+  switch(opcion)
+  {
+    default:
+      respuesta = 0;
+      break;
+    case 1:
+      respuesta = suma(a,b);
+      break;
+    case 2:
+      respuesta = resta(a,b);
+      break;
+    case 3:
+      respuesta = mult(a,b);
+      break;
+    case 4:
+      respuesta = div(a,b);
+      break;
+    case 5:
+      respuesta = exp(a,b);
+      break;
+  }
+  printf("El resultado de la operacion es: %d\n", respuesta);
+  return 0;
+}
